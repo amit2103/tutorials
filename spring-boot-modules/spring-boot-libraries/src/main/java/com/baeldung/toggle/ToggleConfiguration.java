0b77a03c2bcc2950218cfe7eb11a9ef2747f2a25
Amit Pandey <amitpandey@cloud.upwork.com>
2024-07-28 20:09:59 +0530

    JAVA-37798 :- Initial commit

--- spring-boot-modules/spring-boot-libraries/src/main/java/com/baeldung/toggle/ToggleConfiguration.java
+++ spring-boot-modules/spring-boot-libraries/src/main/java/com/baeldung/toggle/ToggleConfiguration.java
@@ -1,20 +1,32 @@
 package com.baeldung.toggle;
 
+import jakarta.persistence.EntityManagerFactory;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.AdviceMode;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.togglz.core.manager.EnumBasedFeatureProvider;
 import org.togglz.core.spi.FeatureProvider;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableJpaRepositories("com.baeldung.toggle")
 @EntityScan("com.baeldung.toggle")
+@EnableTransactionManagement(mode= AdviceMode.ASPECTJ)
 public class ToggleConfiguration {
 
     @Bean
     public FeatureProvider featureProvider() {
         return new EnumBasedFeatureProvider(MyFeatures.class);
     }
-
 }
